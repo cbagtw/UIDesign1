f22d31142e407cd49f42237e7cfa1d0b65093fe6
cbagtw <87054377+cbagtw@users.noreply.github.com>
2024-06-25 13:46:30 +0900

    added projects to personal web

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.querySelector('#darkmode').addEventListener('click', function() {
       document.getElementsByTagName('body')[0].classList.toggle('dark');
 });
+
+function updateClock() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var formattedTime = currentTime.toLocaleString();
+
+  // Add leading zero to minutes and seconds if needed
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  var message = hours + ":" + minutes + ":" + seconds;
+  document.getElementById("clock").innerText = message;
+}
+window.onload = function() {
+  updateClock(); // Initial call to display the time immediately
+  setInterval(updateClock, 1000); // Update the clock every second
+}
